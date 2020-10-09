63ba5e9b6669e656af8d403e62a6a270488b55fe
gzmarkchoi <gzmarkchoi@gmail.com>
2020-10-09 23:37:07 +0200

    - Josephus popKid()

--- src/com/mci/linkedlist/JosephusDemo.java
+++ src/com/mci/linkedlist/JosephusDemo.java
@@ -4,8 +4,12 @@ public class JosephusDemo {
 
 	public static void main(String[] args) {
 		CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
-		circleSingleLinkedList.addKid(5); // a 5 kid list
+		circleSingleLinkedList.addKid(25);
 		circleSingleLinkedList.showKid();
+		
+		// Test kids pop order, it should be 2->4->1->5->3 for a 5 kid circle
+		circleSingleLinkedList.popKid(1, 2, 25);
+		
 	}
 
 }
@@ -13,7 +17,7 @@ public class JosephusDemo {
 class CircleSingleLinkedList {
 	private Kid first = null;
 	
-	// add a number of¨¦ kids
+	// add a number of kids
 	public void addKid(int numberOfKids) {
 		if (numberOfKids < 1) {
 			System.out.println("Number of kids not correct");
@@ -51,6 +55,49 @@ class CircleSingleLinkedList {
 			}
 			currentKid = currentKid.getNext();
 		}
+	}
+	
+	// Calculate kid pop order
+	/**
+	 * @param startNum start from which kid number 
+	 * @param countNum number of count
+	 * @param total total number of kids in the circle
+	 */
+	public void popKid (int startNum, int countNum, int total) {
+		// Check all input numbers
+		if (first == null || startNum < 1 || startNum > total) {
+			System.out.println("Input numbers incorrect");
+		}
+		// Helper pointer
+		Kid helper = first;
+		
+		while(true) {
+			if (helper.getNext() == first) { // helper point to the last kid
+				break;
+			}
+			helper = helper.getNext();
+		}
+		// Before the kid start counting, move first and helper (startNum -1) kids 
+		for (int i = 0; i < startNum -1; i++) {
+			first = first.getNext();
+			helper = helper.getNext();
+		}
+		while(true) {
+			if (helper == first) { // only 1 kid in the circle
+				break;
+			}
+			// Let helper and first move (countNum -1) kids
+			for (int i = 0; i < countNum -1; i++) {
+				first = first.getNext();
+				helper = helper.getNext();
+			}
+			// "first" point to the to the kid that pops
+			System.out.printf("kid %d poped\n", first.getNumber());
+			// get rid of the kid in the circle
+			first = first.getNext();
+			helper.setNext(first);
+		}
+		System.out.printf("Last kid number %d stays\n", helper.getNumber());
 	}
 }
 
